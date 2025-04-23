9a43088f457a1587718a60aca3f4f430ad173d04
baesee <anekddl7@naver.com>
2025-04-23 17:11:07 +0900

    study : #4.3 buildContext의 이해

--- lib/main.dart
+++ lib/main.dart
@@ -12,58 +12,37 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  int counter = 0;
-  List<int> numbers = [];
-
-  void onClicked(String type) {
-    setState(() {
-      if (type == 'plus') {
-        counter = counter + 1;
-        numbers.add(numbers.length);
-      } else if (type == 'minus') {
-        counter = counter - 1;
-        numbers.removeLast();
-      }
-      print('넘버스 : $numbers');
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: const Color(0xFFF4EDDB),
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(color: Colors.red, fontSize: 10),
+        ),
+      ),
+      home: const Scaffold(
+        backgroundColor: Color(0xFFF4EDDB),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Click Count',
-                style: TextStyle(color: Colors.red, fontSize: 20),
-              ),
-              // Text(
-              //   '$counter',
-              //   style: const TextStyle(color: Colors.black, fontSize: 20),
-              // ),
-              for (var n in numbers) Text('$n'),
-              IconButton(
-                onPressed: () => onClicked('plus'),
-                iconSize: 40,
-                icon: const Icon(Icons.add_box_rounded),
-              ),
-              IconButton(
-                onPressed: () => onClicked('minus'),
-                iconSize: 40,
-                icon: const Icon(Icons.remove_circle_rounded),
-              ),
-              IconButton(
-                onPressed: () => onClicked('stop'),
-                iconSize: 40,
-                icon: const Icon(Icons.stop_circle_rounded),
-              ),
-            ],
+            children: [MyLargeTitle()],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class MyLargeTitle extends StatelessWidget {
+  const MyLargeTitle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'My Large Ttile',
+      style: TextStyle(
+        fontSize: 30,
+        color: Theme.of(context).textTheme.titleLarge?.color,
       ),
     );
   }
