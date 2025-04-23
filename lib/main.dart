ed58dcef1fcba6bd0a357af93efa3405a97fd4f8
baesee <anekddl7@naver.com>
2025-04-23 17:52:44 +0900

    study : #4.4 widget lifecycle( initState(), dispose() )

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,14 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  bool isVisible = true;
+
+  void toggleVisibility() {
+    setState(() {
+      isVisible = !isVisible;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,12 +28,18 @@ class _AppState extends State<App> {
           titleLarge: TextStyle(color: Colors.red, fontSize: 10),
         ),
       ),
-      home: const Scaffold(
-        backgroundColor: Color(0xFFF4EDDB),
+      home: Scaffold(
+        backgroundColor: const Color(0xFFF4EDDB),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [MyLargeTitle()],
+            children: [
+              isVisible ? const MyLargeTitle() : const Text('Nothing'),
+              IconButton(
+                onPressed: toggleVisibility,
+                icon: const Icon(Icons.remove_red_eye),
+              ),
+            ],
           ),
         ),
       ),
@@ -33,11 +47,35 @@ class _AppState extends State<App> {
   }
 }
 
-class MyLargeTitle extends StatelessWidget {
+class MyLargeTitle extends StatefulWidget {
   const MyLargeTitle({super.key});
 
   @override
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
+}
+
+class _MyLargeTitleState extends State<MyLargeTitle> {
+  // 여기서 작성을 해도 초기화의 의미.
+
+  @override
+  void initState() {
+    // 빌드 메서드 보다 무조건 먼저 실행, 그리고 최초 1번만 실행됨.
+    // 굳이 필요하지 않을 수도 있다.
+    print('initState');
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // 위젯이 사라질때 호출되는 메서드
+    // 무언가를 취소할때 사용 될 수 있겟다.
+    print('dispose');
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print('build');
     return Text(
       'My Large Ttile',
       style: TextStyle(
